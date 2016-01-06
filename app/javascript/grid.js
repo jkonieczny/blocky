4541eac6aa17f0c9773ce29f35c435465f8a575c
Jan Konieczny <jkonieczny@gmail.com>
2016-01-06 00:11:14 +0000

    First take on the test

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -3,6 +3,7 @@
 const COLOURS = ['red', 'green', 'blue', 'yellow'];
 const MAX_X = 10;
 const MAX_Y = 10;
+const EMPTY = "white";
 
 class Block {
     constructor (x, y) {
@@ -53,7 +54,66 @@ class BlockGrid {
     }
 
     blockClicked (e, block) {
-        console.log(e, block);
+        console.log(e,block);
+
+        this.markNeighboursAsEmpty(block.x,block.y,block.colour);
+        this.moveEmpty();
+        this.clearGrid();
+        this.render();
+    }
+
+    // helper method to make blockClicked more readable
+    clearGrid(){
+        document.querySelector('#gridEl').innerHTML = '';
+    }
+
+    // helper method - check if x,y coordinates are inside grid to avoid errors
+    // extracted to make markNeighboursAsEmpty more readable
+    blockInsideGrid(x,y){
+        return  x >= 0 &&
+                y >= 0 &&
+                x < MAX_X &&
+                y < MAX_X;
+    }
+
+    // helper method to decide if the block needs to be processed
+    // extracted to make markNeighboursAsEmpty more readable
+    matchingBlock(x,y, colour){
+        return this.grid[x][y].colour == colour &&
+               this.grid[x][y].colour != EMPTY
+    }
+
+    // recursive method to process matching neighbours
+    // this won't scale well with large grid and big clusters of same colour
+    markNeighboursAsEmpty(x,y, colour){
+        if(this.blockInsideGrid(x,y) &&
+           this.matchingBlock(x,y,colour) ){
+
+           this.grid[x][y].colour = EMPTY;
+
+           this.markNeighboursAsEmpty(x-1,y, colour);
+           this.markNeighboursAsEmpty(x+1,y, colour);
+           this.markNeighboursAsEmpty(x,y+1, colour);
+           this.markNeighboursAsEmpty(x,y-1, colour);
+        }
+    }
+
+    // bubble sort of block colours to make sure 'non-empty' blocks fall down:
+    moveEmpty(){
+        for(var i=0;i<this.grid.length;i++){
+            var swapped;
+            do {
+                swapped = false;
+                for (var j=0; j < this.grid[i].length-1; j++) {
+                    if (this.grid[i][j].colour == EMPTY && this.grid[i][j+1].colour != EMPTY) {
+                        var temp = this.grid[i][j].colour;
+                        this.grid[i][j].colour = this.grid[i][j+1].colour;
+                        this.grid[i][j+1].colour = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
     }
 }
 
